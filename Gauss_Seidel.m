2f2e6e075850af720db3e0c5d441551e40bb655b
anruki <79452048+anruki@users.noreply.github.com>
2023-03-17 13:17:07 +0100

    Update Gauss_Seidel.m

--- Gauss_Seidel.m
+++ Gauss_Seidel.m
@@ -13,7 +13,9 @@ function [x, itr] = Gauss_Seidel(A,b,tol)
         disp("ERROR: la matriz no es cuadrada")
         return
     end
-    % Paso 2: comprobamos si la matriz es diagonalmente dominante
+    % Paso 2: comprobamos si la matriz es diagonalmente dominante.
+    % Si los valores absolutos de la diag son mayores que el valor absoluto de la suma de cada
+    % columna, entonces es diagonalmente dominante.
     sum = 0;
     for i = 1:F
         for j=1:F
@@ -21,7 +23,9 @@ function [x, itr] = Gauss_Seidel(A,b,tol)
                 sum = sum + A(i,j);
             end
         end
-        if abs(sum) > abs(A(i,i))
+        % Se calcula el error relativo como el valor absoluto de la
+        % diferencia de la sol2 - sol1 entre la sol1
+        if abs(sum) > abs(A(i,i))     
             disp("ERROR: la matriz no es diagonalmente dominante");
             return
         end
@@ -39,9 +43,8 @@ function [x, itr] = Gauss_Seidel(A,b,tol)
         for i=1:F
             x(i) = (1/A(i,i))*(b(i) - A(i,[1:i-1,i+1:F])*x([1:i-1,i+1:F]));
         end
+        % Se suma 1 a itr por cada iteración
         itr = itr + 1;
-        % fprintf("Iteracion numero %d",itr);
-        % x
     end
 
 end
