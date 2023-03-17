cb34868c06cdc4eba14698140d45fb9c057f9ee6
anruki <79452048+anruki@users.noreply.github.com>
2023-03-17 15:56:01 +0100

    Update SubsAtras.m

--- SubsAtras.m
+++ SubsAtras.m
@@ -1,18 +1,19 @@
 function y = SubsAtras(A,b)
-% Función que realiza la sustitución hacia atrás tras descomponer una
-% matriz de coeficientes
-% Input: 
-%       - A: matriz de coeficientes del sistema 
-%       - b: matriz de términos independientes del sistema
-% Output: 
-%       - y: matriz columna que sirve como la nueva matriz de términos
-%       independientes
-
-n = size(A, 1);
-y = zeros(n, 1);
-
-y(n) = b(n) / A(n, n);
-for i = n-1:-1:1
-    y(i) = (b(i) - A(i, i+1:n)*y(i+1:n)) / A(i, i);
-end
+    % Función que utiliza el método de sustitución hacia atrás
+    % para obtener la solución de un sistema cuya matriz de
+    % coeficientes es triangular inferior. 
+    % INPUTS:
+    %   A = matriz triangular inferior
+    %   b = vector columna de términos independientes
+    % OUTPUTS
+    %   y = vector columna solución del sistema
+    Ub=[A,b];
+    [f, c] = size(Ub);
+    y = zeros(f, 1);
+    % Despejamos la última componente de la solución
+    y(f) = Ub(f, c) / Ub(f, f);
+    for i = f-1:-1:1
+        % Se realiza sustitución hacia atrás
+        y(i)= (Ub(i, c) - Ub(i, i+1:f)*y(i+1:f)) / Ub(i, i);
+    end
 end
