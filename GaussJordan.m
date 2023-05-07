fab19811eaea745c20b41ec6b32b32b8d2829fc8
Alf-caput <alfre.roble.aba@gmail.com>
2023-05-07 15:35:52 +0200

    sustituida funcion gaussjordan

--- GaussJordan.m
+++ GaussJordan.m
@@ -1,33 +1,46 @@
-function x = GaussJordan(A,b)
-% Función que permite calcular la solución aproximada de un sistema lineal
-% mediante el método Gauss Jordan con pivote.
-% El sistema Ax=b introducido tiene que ser compatible determinado.
-% Inputs: 
-%       - A: matriz de coeficientes
-%       - b: vector columna de términos independientes
-% Outputs:
-%       - x: vector columna solución aproximada
-    
-% Se define la matriz ampliada
+function x = GaussJordan(A, b)
+% Resuelve un sistema de ecuaciones lineales Ax = b mediante el método de
+% Gauss-Jordan con pivoteo parcial.
+%
+% Variables de entrada:
+%   A = matriz de tamaño n x n
+%   b = vector columna de tamaño n
+%
+% Variables de salida:
+%   x = vector columna solución de tamaño n
+
+% Cuerpo de la función:
 Ab = [A, b];
-[f, c] = size(Ab);
-for j = 1: f
-    % Hallamos el pivote que tiene valor absoluto mayor
-    [pivote, pos] = max(abs(Ab(j:f, j)));
-    % Intercambiamos filas para mayor pivote
-    Ab([j, pos+j-1], :) = Ab([pos+j-1, j], :);
-    % Se normaliza la ecuación pivotante
-    Ab(j, j:c) = Ab(j, j:c) /  pivote;
-    % Se hace ceros encima del pivote
-    for k = j-1:-1:1
-        Ab(k, j:c) = Ab(k, j:c) -  Ab(k, j) * Ab(j, j:c);
+[F, C] = size(Ab);
+
+for j = 1:C-1 % Contador de las columnas
+    % Elegimos fila con mayor pivote:
+    p = abs(Ab(j,j));    % p = pivote de la columna
+    fp = j;              % fp = indice de la fila del pivote
+
+    % Se encuentra el pivote:
+    for i = j+1:F
+        if abs(Ab(i, j)) > p
+            p = abs(Ab(i, j));
+            fp = i;
+        end
     end
-    % Se hace ceros debajo del pivote
-    for i = j+1:f
-        Ab(i, j:c) = Ab(i, j:c) -  Ab(i, j) * Ab(j, j:c);
+
+    % Se intercambian filas "j" y "p":
+    v = Ab(j,:);
+    Ab(j,:) = Ab(fp,:);
+    Ab(fp,:) = v;
+
+    % Se reducen las filas utilizando el pivote:
+    Ab(j,:) = Ab(j,:)/Ab(j,j);
+    for i = 1:F % contador de filas por pivote
+        if Ab(i,j) ~= 0 && i ~= j % Compruebo si el elemento ya es nulo    
+            Ab(i,:) = Ab(i,:) - Ab(i,j) * Ab(j,:);
+        end
     end
 end
 
-% La columna de términos independientes es la solución
-x = Ab(:, c);
+% Se encuentra la solución:
+x = Ab(:,C);
+
 end
