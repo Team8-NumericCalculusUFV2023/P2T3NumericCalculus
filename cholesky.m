3caec087f1d417f0e3b4f3a7e6b8a2965da68320
Alf-caput <alfre.roble.aba@gmail.com>
2023-05-07 16:28:54 +0200

    cholesky termina en end

--- cholesky.m
+++ cholesky.m
@@ -6,28 +6,29 @@ function G = Cholesky(A)
 % Output: 
 %       - G: matriz descompuesta por Cholesky
 
-% Verificar que A es una matriz cuadrada y simétrica
-[n,m] = size(A);
-if n ~= m || ~isequal(A,A')
-    error('No es cuadrada y/o simétrica');
-end
-
-% Verificar que A es definida positiva
-if any(eig(A) <= 0)
-    error('No es definida positiva');
-end
-
-G = triu(n);
-
-for j=1:n
-    for k=1:j-1
-        A(j,j) = A(j,j) - G(k,j)^2;
+    % Verificar que A es una matriz cuadrada y simétrica
+    [n,m] = size(A);
+    if n ~= m || ~isequal(A,A')
+        error('No es cuadrada y/o simétrica');
     end
-    G(j,j) = sqrt(A(j,j));
-    for i=j+1:n
+    
+    % Verificar que A es definida positiva
+    if any(eig(A) <= 0)
+        error('No es definida positiva');
+    end
+    
+    G = triu(n);
+    
+    for j=1:n
         for k=1:j-1
-            A(i,j) = A(i,j) - G(k,i)*G(k,j);
+            A(j,j) = A(j,j) - G(k,j)^2;
         end
-        G(j,i) = A(i,j)/G(j,j);
+        G(j,j) = sqrt(A(j,j));
+        for i=j+1:n
+            for k=1:j-1
+                A(i,j) = A(i,j) - G(k,i)*G(k,j);
+            end
+            G(j,i) = A(i,j)/G(j,j);
+        end
     end
 end
